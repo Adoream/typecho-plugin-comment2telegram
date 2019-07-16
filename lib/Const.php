fe88c6414611196db2b5341e9b2a58f820b6e54e
MoeLoli <moeprprpr@gmail.com>
2019-07-16 09:09:37 +0800

    无关紧要

--- lib/Const.php
+++ lib/Const.php
@@ -1,6 +1,8 @@
 <?php
 class Plugin_Const {
-    const VERSION = '1.1.7';
+    const VERSION = '1.2.0';
     
     const EMAIL_SENT_API = 'https://api.aim.moe/SendEmail';
+
+    const GITHUB_REPO_API = 'https://api.github.com/repos/MoeLoli/Comment2Telegram/releases/latest';
 }
