12bc81ef748fafda26e526f3104afbb0d668641c
Jin <moeprprpr@gmail.com>
2020-03-07 12:47:00 +0800

    Update Const.php

--- lib/Const.php
+++ lib/Const.php
@@ -2,7 +2,7 @@
 class Plugin_Const {
     const VERSION = '1.3.2';
     
-    const EMAIL_SENT_API = 'https://api.aim.moe/SendEmail';
+    const EMAIL_SENT_API = 'https://api.aim.moe/Common/SendEmail';
 
     const GITHUB_REPO_API = 'https://api.github.com/repos/MoeLoli/Comment2Telegram/releases/latest';
 }
